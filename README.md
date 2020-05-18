a5e580b129ddb97ba02f2d8a5dd02f87162b5617
Annalisa Pagnozzi <annalisa.pagnozzi@gmail.com>
2020-05-18 12:20:59 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # entity_extractor
 
-prova
+extract_wiki_page_links.py = given name of wikipedia page, extracts links of entities from wikipedia page's html
+
+extract_dbpedia_properties= given name of wikipedia entity (similar to wikipedia page) extracts meaningful properties of name, in the form of a dictionary.
+
+match_dbpedia_properties_with_wiki_links: returns all the properties that are matched from wikipedia page and dpbedia properties. NOTE: most of the properties don't have a representing text that should be tagged, so maybe create a string generator to add the content to the wikipedia text?
+
+
 
 
--- entity_extractor.py
+++ entity_extractor.py
@@ -33,7 +33,7 @@ def extract_test_data():
     f = open("./dict_companies_html_test.json", "w+")
     json_data = json.dumps(dict_test)
     f.write(json_data)  
-extract_test_data()
+#extract_test_data()
 
 def extract_dbpedia_properties(company_name):
     metadata = ["wikiPageWikiLink", "wikiPageID", "wikiPageExternalLink", "wikiPageRevisionID", "abstract"]
@@ -49,16 +49,15 @@ def extract_dbpedia_properties(company_name):
 
             record_property = record_property.replace("http://dbpedia.org/ontology/", "")
             record_value = record_value.replace("http://dbpedia.org/resource/", "")
-            dict_properties_values[record_property] = record_value
+            dict_properties_values[record_value] = record_property
     
     return dict_properties_values
-
 
 
 def extract_wiki_page_links(html_doc):
     entities_links = []
     soup = BeautifulSoup(html_doc, 'html.parser')
-    metadata = ["Wikipedia:", "Portal:", "Category:", "Help:"]
+    metadata = ["Wikipedia:", "Portal:", "Category:", "Help:", "Template:", "Template_talk"]
     links = soup.find_all('a')
     for link in links:
         if "<a href=\"/wiki/" in str(link):
@@ -71,12 +70,21 @@ def extract_wiki_page_links(html_doc):
     
     return entities_links
 
+def match_dbpedia_properties_with_wiki_links(wiki_links, dbproperties):
+    for link in wiki_links:
+        entity_value = link.get("href").replace("/wiki/", "")
+        entity_title = link.get("title")
+    
+        if entity_value in dbproperties:
+            print(entity_value+" : "+ dbproperties[entity_value])
+
+    
 def main():
 
     company_name = "Luke Records"
     html_doc = dbpedia_companies_html[company_name]
     legit_wiki_links = extract_wiki_page_links(html_doc)
     dbpedia_properties = extract_dbpedia_properties(company_name)
-    print(dbpedia_properties)
+    match_dbpedia_properties_with_wiki_links(legit_wiki_links, dbpedia_properties)
 
 main()
